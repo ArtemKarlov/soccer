f891af0efdc06ee97d564245f0cfab2683ed1f8d
ArtemKarlov <ar.karlov@gmail.com>
2021-01-20 11:01:13 +0400

    layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
