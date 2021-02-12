d0cceb6a7069cb4d0ae411b953ffb5435611a4a0
ArtemKarlov <ar.karlov@gmail.com>
2021-02-12 13:52:10 +0400

    get parasms

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Filters(props) {
 
@@ -13,12 +14,12 @@ export default function Filters(props) {
         minDateLimit.setDate(minDateLimit.getDate() - dateRange);
     const formatedMinDateLimit = getFormatedDate(minDateLimit);
 
-    console.log(maxDateLimit);
-
     function getFormatedDate(date) {
         const formatedDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
         return formatedDate;
     }
+
+    const location = useLocation();
 
     return (
         <div className="p-2 bg-gray-400 rounded flex justify-start items-center space-x-3">
@@ -34,11 +35,16 @@ export default function Filters(props) {
                     onChange={(event) => setDateTo(event.target.value)}
                 />
             </label>
-            <button className="py-1 px-3 rounded text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-800 focus:outline-none"
+            {/* <button className="py-1 px-3 rounded text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-800 focus:outline-none"
                 onClick={() => {props.sendDateRange(dateFrom, dateTo)}}
             >
                 Search
-            </button>
+            </button> */}
+            <Link to={`${location.pathname}?dateFrom=${dateFrom ? dateFrom : dateTo ? formatedMinDateLimit : ''}&dateTo=${dateTo ? dateTo : dateFrom ? formatedMaxDateLimit : ''}`} className="py-1 px-4 rounded space-x-4 flex justify-start items-center bg-blue-400 hover:bg-blue-500 active:bg-blue-800" 
+                 onClick={() => {props.handleClick()}}
+            >
+                Search
+            </Link>
         </div>
     );    
 }
--- src/components/MatchDay/MatchDay.jsx
+++ src/components/MatchDay/MatchDay.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams, useRouteMatch } from "react-router-dom";
 
 import Match from "../Match/Match.jsx";
 import Filters from "../Filters/Filters.jsx";
@@ -7,7 +7,6 @@ import Filters from "../Filters/Filters.jsx";
 import { API_HOST } from "../global/global.jsx";
 
 function MatchDay(props) {
-
     const [matches, setMatches] = useState([]);
     const [isMatchesLoaded, setIsMatchesLoaded] = useState(false);
 
@@ -15,6 +14,7 @@ function MatchDay(props) {
     const { teamId } = useParams();
     const currentTeam = teams.find((team) => team.id === parseInt(teamId, 10));
 
+    const location = useLocation();
     const matchesUrl = new URL(`v2/teams/${teamId}/matches`, API_HOST);
 
     function getData(url) {
@@ -25,21 +25,24 @@ function MatchDay(props) {
             },
         })
         .then((response) => {
+
             return response.json();
         })
         .then((data) => {
             setMatches([...data.matches]);
         });        
     }
-
-    const getDateFilteredData = (start, end) => {
-        matchesUrl.searchParams.append("dateFrom", start);
-        matchesUrl.searchParams.append("dateTo", end);
-        getData(matchesUrl);
+    
+    const handleFilter = () => { 
+        setIsMatchesLoaded(false);  
     }
     
     useEffect(() => {
         if (!isMatchesLoaded) {
+            const dateFrom = (new URLSearchParams(location.search).get("dateFrom"));
+            const dateTo = (new URLSearchParams(location.search).get("dateTo"));
+            matchesUrl.searchParams.append("dateFrom", dateFrom !== null ? dateFrom : '');
+            matchesUrl.searchParams.append("dateTo", dateTo !== null ? dateTo : '');
             getData(matchesUrl);
             setIsMatchesLoaded(true);
         }  
@@ -47,7 +50,7 @@ function MatchDay(props) {
 
     return (
         <Fragment>
-            <Filters sendDateRange={getDateFilteredData} />
+            <Filters handleClick={handleFilter} />
             <div className="my-4 bg-gray-400 rounded">
                 <div className="pt-4 px-10">
                     <p className="font-bold text-xl text-gray-800 text-center">{currentTeam.name}</p>
