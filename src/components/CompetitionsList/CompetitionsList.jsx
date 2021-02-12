05563caccb6aee2567c525f083419ea7ec36eee7
ArtemKarlov <ar.karlov@gmail.com>
2021-02-12 20:20:38 +0400

    add yearFilter

--- src/components/CompetitionsList/CompetitionsList.jsx
+++ src/components/CompetitionsList/CompetitionsList.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 import Competition from "../Competition/Competition.jsx";
+import YearFilter from "../Filters/YearFilter.jsx";
 
 
 export default function CompetitionsList(props) {
     const {leagueList} = props;
 
     return (
-        <ul className="max-w-md mx-auto p-2">
-            { leagueList.map((competition) => 
-                <li key={competition.id} className="my-2">
-                    <Link to={`/leagues/${competition.id}/calendar`} className="py-2 px-4 space-x-4 flex justify-start items-center bg-gray-600 rounded hover:bg-gray-700 active:bg-gray-800">
-                        <Competition competition={competition} />
-                    </Link>
-                </li>
-            )}                            
-        </ul>
+        <Fragment>
+            <YearFilter />
+            <ul className="max-w-md mx-auto p-2">
+                { leagueList.map((competition) => 
+                    <li key={competition.id} className="my-2">
+                        <Link to={`/leagues/${competition.id}/calendar`} className="py-2 px-4 space-x-4 flex justify-start items-center bg-gray-600 rounded hover:bg-gray-700 active:bg-gray-800">
+                            <Competition competition={competition} />
+                        </Link>
+                    </li>
+                )}                            
+            </ul>
+        </Fragment>
     );
 }
---
+++ src/components/Filters/YearFilter.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function YearFilter(props) {
+
+    const handleSelect = (event) => {
+        console.log(event.target.value);
+    }
+
+    const currentYear = (new Date()).getFullYear();
+    console.log(currentYear);
+    const optionYearPool = [];
+    for (let i = (currentYear - 30); i <= currentYear; i++) {
+        if (i !== currentYear) {
+            optionYearPool.push(<option value={i}>{i}</option>);    
+        } else {
+            optionYearPool.push(<option value={i} selected>{i}</option>);
+        }
+    }
+
+    return (
+        <div className="p-2 bg-gray-400 rounded flex justify-start items-center space-x-3">
+            <select name="year" size="1" className="h-8 p-1 pr-10 text-gray-800"
+                onChange={(e) => handleSelect(e)}
+            >
+                { optionYearPool }
+            </select>
+        </div>
+    );
+}
